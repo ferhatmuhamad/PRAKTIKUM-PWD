514280deb51b49daa27a479e379da9c0a16a4a78
ferhat muhamad <80558836+ferhatmuhamad@users.noreply.github.com>
2021-10-05 22:26:01 +0700

    Add files via upload

---
+++ latihan1_test.php
@@ -0,0 +1,8 @@
+<?php
+$myDir = "c:/xampp/htdocs/pwd/1800018221";
+$dir = opendir($myDir);
+echo "Isi folder (klik link untuk download : <br>";
+while ($tmp = readdir($dir)) {
+    echo "<a href='$tmp' target='_blank'>$tmp</a><br>";
+}
+closedir($dir);
